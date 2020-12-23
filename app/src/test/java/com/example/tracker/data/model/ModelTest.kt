10a228098a33c631235533d78f45440592cafd2c
xrseventy <5526958@gmail.com>
2020-12-23 19:16:01 +0300

    presenter tests

--- app/src/test/java/com/example/tracker/data/model/ModelTest.kt
+++ app/src/test/java/com/example/tracker/data/model/ModelTest.kt
@@ -20,6 +20,7 @@ class ModelTest {
 
     private val preferencesProviderMock: PreferencesProvider = mock()
     private val model: Model = Model(preferencesProviderMock)
+
     @Test
     fun `putString is invoked`() {
         var putStrMock = preferencesProviderMock.putString(KEY_STR_SAVED_WALK, "String")
@@ -32,22 +33,22 @@ class ModelTest {
     @Test
     fun `getString for first launch app emptyList`() {
         whenever(preferencesProviderMock.getString(KEY_STR_SAVED_WALK)).thenReturn("")
-        val testResult: String? =  model.getListWalksFromSharedPref()
-        assertEquals("", testResult )
+        val testResult: String? = model.getListWalksFromSharedPref()
+        assertEquals("", testResult)
     }
 
     @Test
     fun `getString for second launch app`() {
         whenever(preferencesProviderMock.getString(KEY_STR_SAVED_WALK)).thenReturn("Kolomentskoe, 5.0")
-        val testResult: String? =  model.getListWalksFromSharedPref()
-        assertEquals("Kolomentskoe, 5.0", testResult )
+        val testResult: String? = model.getListWalksFromSharedPref()
+        assertEquals("Kolomentskoe, 5.0", testResult)
     }
 
     @Test
     fun `getString for null list`() {
         whenever(preferencesProviderMock.getString(KEY_STR_SAVED_WALK)).thenReturn(null)
-        val testResult: String? =  model.getListWalksFromSharedPref()
-        assertEquals(null, testResult )
+        val testResult: String? = model.getListWalksFromSharedPref()
+        assertEquals(null, testResult)
     }
 
     @Test
@@ -58,10 +59,11 @@ class ModelTest {
 
     @Test
     fun `hasKey without key`() {
-       whenever(preferencesProviderMock.hasKey("")).thenReturn(false)
-       model.checkKeySavedWalksList()
+        whenever(preferencesProviderMock.hasKey("")).thenReturn(false)
+        model.checkKeySavedWalksList()
     }
-    companion object {
-        const val KEY_STR_SAVED_WALK = "SavedWalk"}
 
+    companion object {
+        const val KEY_STR_SAVED_WALK = "SavedWalk"
+    }
 }
--- app/src/test/java/com/example/tracker/presenter/TrackerPresenterTest.kt
+++ app/src/test/java/com/example/tracker/presenter/TrackerPresenterTest.kt
@@ -1,92 +1,35 @@
 package com.example.tracker.presenter
 
-import com.example.tracker.data.SavedWalk
 import com.example.tracker.data.model.Model
-import com.example.tracker.data.model.ModelWalksScreenState
 import com.example.tracker.view.TrackerView
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.*
 import org.junit.Test
 
-import org.junit.Assert.*
-
 class TrackerPresenterTest {
+
     private val trackerViewMock: TrackerView = mock()
-    private val modelMock :Model = mock()
-    private val testObjectWalk: SavedWalk = SavedWalk("Kolomentskoe", 5.0)
-    private val testListWalk: MutableList<SavedWalk> = mutableListOf(testObjectWalk)
-
-    private var modelWalksScreenStateMock: ModelWalksScreenState =
-        ModelWalksScreenState("Alice's wood", "7.0", testListWalk , false)
+    private val modelMock: Model = mock()
+    private val trackerPresenter: TrackerPresenter = TrackerPresenter(trackerViewMock, modelMock)
 
     @Test
-    fun `init invoke renderView `() {
-        var tackerViewMock = trackerViewMock.renderView(modelWalksScreenStateMock)
-        verify(trackerViewMock).renderView(modelWalksScreenStateMock)
+    fun `valid cases clickAddButton`() {
+        trackerPresenter.onLocationTextChanged("Sokolniki")
+        trackerPresenter.onDistanceTextChanged("8.0")
+        trackerPresenter.clickAddButton()
+        verify(modelMock).addWalk(any())
+        verify(modelMock).saveSharedPref(any())
+        verify(trackerViewMock).renderView(any())
     }
 
     @Test
-    fun `success updateModelWalks after click `() {
-
-        updateModelWalksTest("Alice's wood", "7.0", testListWalk , false)
-        val expectModelWalksScreenStateMock: ModelWalksScreenState =
-            ModelWalksScreenState("Alice's wood", "7.0", testListWalk , false)
-        assertEquals( expectModelWalksScreenStateMock.enterLocation, modelWalksScreenStateMock.enterLocation)
-        assertEquals( expectModelWalksScreenStateMock.enterDistance, modelWalksScreenStateMock.enterDistance)
-        assertEquals( expectModelWalksScreenStateMock.listWalks, modelWalksScreenStateMock.listWalks)
-        assertEquals( expectModelWalksScreenStateMock.areErrorsVisible, modelWalksScreenStateMock.areErrorsVisible)
-    }
-
-    @Test
-    fun `location and distance errors updateModelWalks after click `() {
-        updateModelWalksTest("", "7.0", testListWalk , true)
-        val expectModelWalksScreenStateMock: ModelWalksScreenState =
-            ModelWalksScreenState("", "7.0", testListWalk , true)
-        assertEquals( expectModelWalksScreenStateMock.enterLocation, modelWalksScreenStateMock.enterLocation)
-        assertEquals( expectModelWalksScreenStateMock.enterDistance, modelWalksScreenStateMock.enterDistance)
-        assertEquals( expectModelWalksScreenStateMock.areErrorsVisible, modelWalksScreenStateMock.areErrorsVisible)
-
-    }
-
-    @Test
-    fun `distance error updateModelWalks after click `() {
-        updateModelWalksTest("", "7.0", testListWalk , true)
-        val expectModelWalksScreenStateMock: ModelWalksScreenState =
-            ModelWalksScreenState("", "7.0", testListWalk , true)
-        assertEquals( expectModelWalksScreenStateMock.enterLocation, modelWalksScreenStateMock.enterLocation)
-        assertEquals( expectModelWalksScreenStateMock.enterDistance, modelWalksScreenStateMock.enterDistance)
-        assertEquals( expectModelWalksScreenStateMock.areErrorsVisible, modelWalksScreenStateMock.areErrorsVisible)
-    }
-
-    @Test
-    fun `distance and location error updateModelWalks after click `() {
-        updateModelWalksTest("", "", testListWalk , true)
-        val expectModelWalksScreenStateMock: ModelWalksScreenState =
-            ModelWalksScreenState("", "", testListWalk , true)
-        assertEquals( expectModelWalksScreenStateMock.enterLocation, modelWalksScreenStateMock.enterLocation)
-        assertEquals( expectModelWalksScreenStateMock.enterDistance, modelWalksScreenStateMock.enterDistance)
-        assertEquals( expectModelWalksScreenStateMock.areErrorsVisible, modelWalksScreenStateMock.areErrorsVisible)
-    }
-
-    @Test
-    fun `update enterLocation in ModelWalks`() {
-        val expectInputLocation : String = "Alice's wood"
-        assertTrue(modelWalksScreenStateMock.enterLocation == expectInputLocation)
-   }
-
-    @Test
-    fun `update enterDistance in ModelWalks`() {
-        val expectInputDistance : String = "7.0"
-        assertTrue(modelWalksScreenStateMock.enterDistance == expectInputDistance)
-    }
-
-    private fun updateModelWalksTest(
-        location: String = modelWalksScreenStateMock.enterLocation,
-        distance: String = modelWalksScreenStateMock.enterDistance,
-        listWalks: List<SavedWalk> = modelWalksScreenStateMock.listWalks,
-        areErrorVisible: Boolean = modelWalksScreenStateMock.areErrorsVisible
-    ) {
-        modelWalksScreenStateMock = ModelWalksScreenState(location, distance, listWalks, areErrorVisible)
+    fun `error cases clickAddButton`() {
+        trackerPresenter.onLocationTextChanged("")
+        trackerPresenter.onDistanceTextChanged("1")
+        trackerPresenter.clickAddButton()
+        verify(modelMock, never()).addWalk(any())
+        //another way to do some opposite for verify)
+        //verify(modelMock, Mockito.times(0)).addWalk(any())
+        verify(trackerViewMock).renderView(any())
     }
 
 }
